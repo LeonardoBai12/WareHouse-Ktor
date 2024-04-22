3491ad4315591c6a2be03204748d1462dd392da8
Leonardo Bai <69223422+LeonardoBai12@users.noreply.github.com>
2024-04-22 15:29:13 -0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.23"
+    id("io.ktor.plugin") version "2.3.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23"
+}
+
+group = ""
+version = "0.0.1"
+
+application {
+    mainClass.set(".ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-websockets-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("org.postgresql:postgresql:42.5.1")
+    implementation("com.h2database:h2:2.1.214")
+    implementation("io.ktor:ktor-serialization-gson-jvm")
+    implementation("io.ktor:ktor-server-call-logging-jvm")
+    implementation("io.ktor:ktor-server-double-receive-jvm")
+    implementation("io.ktor:ktor-server-auth-jvm")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
---
+++ src/main/kotlin/plugins/Routing.kt
@@ -0,0 +1,21 @@
+package .plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.plugins.doublereceive.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    install(DoubleReceive)
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        post("/double-receive") {
+            val first = call.receiveText()
+            val theSame = call.receiveText()
+            call.respondText(first + " " + theSame)
+        }
+    }
+}

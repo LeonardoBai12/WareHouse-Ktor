aced1a30bbc04e0df87575aebd08e255ddae903e
Leonardo Bai <69223422+LeonardoBai12@users.noreply.github.com>
2024-05-04 18:19:44 -0300

    Documentation fixes

--- src/main/kotlin/io/lb/warehouse/withdraw/routes/WithdrawRoutes.kt
+++ src/main/kotlin/io/lb/warehouse/withdraw/routes/WithdrawRoutes.kt
@@ -18,20 +18,16 @@ import java.sql.SQLException
  * **Routes documentations:**
  *
  * Create withdraw:
- * [/api/createWithdraw]
- * (https://documenter.getpostman.com/view/28162587/2sA3JGeihC#766c06d9-119c-4a7c-918a-d46bcb0fbd73)
+ * [/api/createWithdraw](https://documenter.getpostman.com/view/28162587/2sA3JGeihC#766c06d9-119c-4a7c-918a-d46bcb0fbd73)
  *
  * Get withdraw by UUID:
- * [/api/withdraw]
- * (https://documenter.getpostman.com/view/28162587/2sA3JGeihC#c65cd374-3703-4d2a-8e6c-28a46a7fc9c5)
+ * [/api/withdraw](https://documenter.getpostman.com/view/28162587/2sA3JGeihC#c65cd374-3703-4d2a-8e6c-28a46a7fc9c5)
  *
  * Get withdraws by user UUID:
- * [/api/withdrawsCreatedByUser]
- * (https://documenter.getpostman.com/view/28162587/2sA3JGeihC#a86ec3ff-a9ad-481f-8bcf-e5fc30da16a2)
+ * [/api/withdrawsCreatedByUser](https://documenter.getpostman.com/view/28162587/2sA3JGeihC#a86ec3ff-a9ad-481f-8bcf-e5fc30da16a2)
  *
  * Get withdraws by ware UUID:
- * [/api/withdrawsByWareId]
- * (https://documenter.getpostman.com/view/28162587/2sA3JGeihC#c9a18637-a85d-42d3-88c7-4e620065b552)
+ * [/api/withdrawsByWareId](https://documenter.getpostman.com/view/28162587/2sA3JGeihC#c9a18637-a85d-42d3-88c7-4e620065b552)
  */
 fun Application.withdrawRoutes(withdrawService: WithdrawDatabaseService) {
     routing {
